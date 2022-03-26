091f3ada5725d80e66940ac3c915e9b731d5f235
IMRANDIL <aliimranadil2@gmail.com>
2022-03-26 17:57:08 +0530

    yh

--- JS/index.js
+++ JS/index.js
@@ -15,6 +15,7 @@ const navigation = document.querySelector('.navigation');
 
 
 const navCloseBtn = document.querySelector('.navigation__close__btn');
+const overlay = document.querySelector('.blur__overlay');
 
 
 
@@ -22,10 +23,18 @@ const isActive = 'is__active';
 
 
 const toggleNav = () => {
-    navigation.classList.toggle(isActive)
+    navigation.classList.toggle(isActive);
+    overlay.classList.toggle(isActive)
 }
 
 
 
 menuBtn.addEventListener('click', toggleNav);
 navCloseBtn.addEventListener('click', toggleNav);
+overlay.addEventListener('click', toggleNav);
+
+
+
+
+
+
