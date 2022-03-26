f58638c1e26f19bbb6034746b030b521cf6dab7d
IMRANDIL <aliimranadil2@gmail.com>
2022-03-26 17:43:20 +0530

    yh

--- JS/index.js
+++ JS/index.js
@@ -2,4 +2,30 @@
 new fullpage('#fullpage', {
     autoScrolling: true,
     scrollBar: true
-})
+});
+
+
+
+
+const menuBtn = document.querySelector('.menu__btn');
+
+
+
+const navigation = document.querySelector('.navigation');
+
+
+const navCloseBtn = document.querySelector('.navigation__close__btn');
+
+
+
+const isActive = 'is__active';
+
+
+const toggleNav = () => {
+    navigation.classList.toggle(isActive)
+}
+
+
+
+menuBtn.addEventListener('click', toggleNav);
+navCloseBtn.addEventListener('click', toggleNav);
